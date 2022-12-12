66332955a6f6a025b31f6a024c1a9f9b1a9c07a7
cristhian-lean-tech <cdelgado@lean-tech.io>
2022-12-12 08:28:08 -0500

    after 3 session

--- api/src/data.js
+++ api/src/data.js
@@ -1,36 +1,37 @@
 export const users = [
-    {
-      id: '1',
-      username: 'Robin Wieruch',
-    },
-    {
-      id: '2',
-      username: 'Dave Davids',
-    },
-    {
-        id: '3',
-        username: 'Juan Gonzales',
-    },
-    {
-        id: '4',
-        username: 'Elvis Crespo',
-    },
+  {
+    id: "1",
+    username: "Robin Wieruch",
+    nickName: "RW",
+  },
+  {
+    id: "2",
+    username: "Dave Davids",
+  },
+  {
+    id: "3",
+    username: "Juan Gonzales",
+  },
+  {
+    id: "4",
+    username: "Elvis Crespo",
+  },
 ];
-  
-  export const messages = [
-    {
-      id: '1',
-      text: 'Hello World',
-      userId: '1',
-    },
-    {
-      id: '2',
-      text: 'By World',
-      userId: '2',
-    },
-    {
-        id: '3',
-        text: 'Another message',
-        userId: '3',
-    }
+
+export const messages = [
+  {
+    id: "1",
+    text: "Hello World",
+    userId: "1",
+  },
+  {
+    id: "2",
+    text: "By World",
+    userId: "2",
+  },
+  {
+    id: "3",
+    text: "Another message",
+    userId: "3",
+  },
 ];
--- api/src/index.js
+++ api/src/index.js
@@ -1,29 +1,43 @@
-import 'dotenv/config';
-import express from 'express';
-import { messages, users } from "./data"
+import "dotenv/config";
+import express from "express";
+import { messages, users } from "./data";
+import cors from "cors";
+import bodyParser from "body-parser";
 
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.send('Hello world');
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+// parse application/json
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  return res.send("Hello world");
 });
 
-app.get('/users', (req, res) => {
-  return res.send(Object.values(users));
+app.get("/users", (req, res) => {
+  // 200/201 -> ok/ok crear
+  // 400 -> fallo desde el usuario, no nos envio el token 401, url no existe 404
+  // 500 -> fallo del servidor
+  console.log("****************");
+  return res.status(200).send(Object.values(users));
 });
 
-app.get('/users/:userId', (req, res) => {
+app.get("/users/:userId", (req, res) => {
   return res.send(users[req.params.userId]);
 });
 
-app.get('/messages', (req, res) => {
+app.get("/messages", (req, res) => {
   return res.send(Object.values(messages));
 });
 
-app.get('/messages/:messageId', (req, res) => {
+app.get("/messages/:messageId", (req, res) => {
   return res.send(messages[req.params.messageId]);
 });
 
 app.listen(process.env.PORT, () =>
-  console.log(`Example app listening on port ${process.env.PORT}!`),
+  console.log(`Example app listening on port ${process.env.PORT}!`)
 );
