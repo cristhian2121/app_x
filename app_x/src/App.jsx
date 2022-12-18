3e526de2050b53a6610c81ed7e8e07f27f145a49
Cesar Fontalvo <cafc2030@gmail.com>
2022-12-18 18:55:39 -0500

    Agregando pagina de detallez

--- app_x/src/App.jsx
+++ app_x/src/App.jsx
@@ -4,12 +4,17 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
 
+//1. hacer la llamada a la api aca en lugar de home
+//2. los id en los datos retornaban el siguiente id
+//3. Como se haria si existen mas mensajes o como mostrar los mensajes en pantalla 
+
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/user/:id" element={<Detail />} />
+        <Route path='*' element={<h1>Not found</h1>} />
       </Routes>
     </>
   );
--- app_x/src/pages/Detail.jsx
+++ app_x/src/pages/Detail.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import { useParams, useNavigate } from "react-router-dom";
+
 
 export default function Detail() {
   // mostrar el usuario actual -> leer los parametros de la url (id)
   // traer los mensajes del usuario si tiene
-  return <div>Details</div>;
+
+  const [user, setUser] = useState([]);
+  const [messages, setMessages] = useState([]);
+
+  const  {id}  = useParams();
+  const userid = id-1;
+  let url = `http://localhost:3100/users/${userid}`;
+  let msgUrl = `http://localhost:3100/messages/${userid}`;
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        //console.log(data);
+        setUser(data);
+      });
+
+      fetch(msgUrl)
+      .then((res) => res.json())
+      .then((data) => {
+        //console.log(data);
+        setMessages(data);
+      });
+  }, []);
+
+  const navigate = useNavigate();
+
+  const backToUsers = () => {
+    navigate(-1);
+  }
+
+  return <>
+    <h2>{`Usuario ${user.username}`} </h2>
+    <p>{user.nickName ? `aka ${user.nickName}`: 'No hay un apodo configurado'}</p>
+    <button onClick={backToUsers}>Atrás</button>
+    <hr />
+    <h2>Mensajes</h2>
+    <ol>
+      <li>{messages.text}</li>
+      {/* { [messages].map(el => <li>{el.text} con id {el.id}</li>) } */}
+    </ol>
+
+  </>;
 }
