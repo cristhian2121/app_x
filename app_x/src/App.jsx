c6b54bcefc71ff7fbbd224d25deef105a371786a
Cristhian <ccdelgadop@gmail.com>
2023-01-20 08:00:17 -0500

    add Wrraper component

--- app_x/src/App.jsx
+++ app_x/src/App.jsx
@@ -14,30 +14,38 @@ import StudentsList from "./pages/StudentsList";
 //2. los id en los datos retornaban el siguiente id
 //3. Como se haria si existen mas mensajes o como mostrar los mensajes en pantalla
 
+function WrapperWithPermission ({ children }) { //HOC
+  return <>
+    <Header />
+    <AuthRoute>
+      {children}
+    </AuthRoute>
+  </>
+}
+
 function App() {
   return (
     <>
       <TitleProvider>
-      <AuthProvider>
+        <AuthProvider>
+          <Routes>
+            <Route path="/" element={<Login />} />
 
-        <Header />
-
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/miuniforme" element={
-            <AuthRoute>
-            <Profile />
-          </AuthRoute>
-          } />
-          <Route path="/estudiantes" element={
-          <AuthRoute>
-          <StudentsList />
-          </AuthRoute>
-          } />
-
-          <Route path="/user/:id" element={<Detail />} />
-          <Route path="*" element={<h1>Not found</h1>} />
-        </Routes>
+            <Route path="/" >
+              <Route path="/miuniforme" element={
+                <WrapperWithPermission>
+                  <Profile />
+                </WrapperWithPermission>
+              } />
+              <Route path="/estudiantes" element={
+                <WrapperWithPermission>
+                  <StudentsList />
+                </WrapperWithPermission>
+              } />
+            </Route>
+            <Route path="/user/:id" element={<Detail />} />
+            <Route path="*" element={<h1>Not found</h1>} />
+          </Routes>
 
         </AuthProvider>
       </TitleProvider>
